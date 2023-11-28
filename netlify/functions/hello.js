1db74bdf4e9b43a75a03a72fa801cd878c393183
elderlyman <ryaneavey@gmail.com>
2023-11-28 14:21:16 -0800

    headers in hello

--- netlify/functions/hello.js
+++ netlify/functions/hello.js
@@ -1,9 +1,14 @@
 // Docs on event and context https://docs.netlify.com/functions/build/#code-your-function-2
+
 exports.handler = async (event) => {
   try {
     const subject = event.queryStringParameters.name || 'World'
     return {
       statusCode: 200,
+      headers: {
+        'Access-Control-Allow-Origin': 'http://localhost:3000',
+        'Content-Type': 'application/json',
+      },
       body: JSON.stringify({ message: `Hello ${subject}` }),
       // // more keys you can return:
       // headers: { "headerName": "headerValue", ... },
