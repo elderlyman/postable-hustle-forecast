3e03f596821a7c7599311643ce1a03b773138824
elderlyman <ryaneavey@gmail.com>
2023-11-28 18:25:41 -0800

    display: block

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
       <header className="App-header">
         {/* <img src={logo} className="App-logo" alt="logo" /> */}
         <WeatherEventsTable
-          style={{ display: "flex" }}
+          style={{ display: "block" }}
         />
         <a
           className="App-link"
