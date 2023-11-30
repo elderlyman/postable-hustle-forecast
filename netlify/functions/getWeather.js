92f3d4b6689296104f6da661ab9086d26816ede1
elderlyman <ryaneavey@gmail.com>
2023-11-30 10:58:05 -0800

    axios in getWeather.js

--- netlify/functions/getWeather.js
+++ netlify/functions/getWeather.js
@@ -1,31 +1,31 @@
-// getWeather.js
-import fetch from 'node-fetch'
-exports.handler = async (event, context) => {
+import axios from 'axios';
 
+const openWeatherApiKey = process.env.REACT_APP_OPENWEATHER_API_KEY || 'default_open_weather_api_key';
+
+exports.handler = async (event, context) => {
     const lat = event.queryStringParameters.lat;
     const lon = event.queryStringParameters.lon;
-    const appid = event.queryStringParameters.appid;
     const exclude = event.queryStringParameters.exclude;
     const units = event.queryStringParameters.units;
 
-
-    if (!lat || !lon || !exclude || !units || !appid) {
-
-        throw new Error('Missing required parameters.'
-            //show missig parameters in error message
-            + ' lat: ' + lat
-            + ' lon: ' + lon
-            + ' exclude: ' + exclude
-            + ' units: ' + units
-            + ' appid: ' + appid
+    if (!lat || !lon || !exclude || !units) {
+        throw new Error(
+            'Missing required parameters.' +
+            ` lat: ${lat}, lon: ${lon}, exclude: ${exclude}, units: ${units}`
         );
-
     }
 
     try {
-        const response = await fetch(
-            `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=${exclude}&units=${units}&appid=${appid}`
-        );
+        const apiUrl = `https://api.openweathermap.org/data/2.5/onecall`;
+        const params = {
+            lat,
+            lon,
+            exclude,
+            units,
+            appid: openWeatherApiKey,
+        };
+
+        const response = await axios.get(apiUrl, { params });
 
         return {
             statusCode: 200,
@@ -33,7 +33,7 @@ exports.handler = async (event, context) => {
                 'Access-Control-Allow-Origin': '*',
                 'Content-Type': 'application/json',
             },
-            body: JSON.stringify(await response.json()),
+            body: JSON.stringify(response.data),
         };
     } catch (error) {
         console.error('Error:', error);
