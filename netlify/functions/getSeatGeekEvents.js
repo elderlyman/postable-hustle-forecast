9904f8304dfd1f67dbb66e36f6598299b8a51eaa
elderlyman <ryaneavey@gmail.com>
2023-11-30 11:02:30 -0800

    remove nide fetch, add axios in getSeatGeekEvents

--- netlify/functions/getSeatGeekEvents.js
+++ netlify/functions/getSeatGeekEvents.js
@@ -1,27 +1,32 @@
 // seatgeekFunction.js
-import fetch from 'node-fetch'
-exports.handler = async (event, context) => {
+import axios from 'axios';
 
+exports.handler = async (event, context) => {
     const eventLocation = event.queryStringParameters['venue.city'];
     const daysFromNow = event.queryStringParameters['datetime_local.lte'];
     const client_id = event.queryStringParameters.client_id;
 
-
     if (!eventLocation || !daysFromNow || !client_id) {
-
-        throw new Error('Missing required parameters.'
-            //show missig parameters in error message
+        throw new Error(
+            'Missing required parameters.'
+            // Show missing parameters in the error message
             + ' eventLocation: ' + eventLocation
             + ' daysFromNow: ' + daysFromNow
             + ' client_id: ' + client_id
         );
-
     }
 
     try {
-
-        const response = await fetch(
-            `https://api.seatgeek.com/2/events?client_id=${client_id}&venue.city=${eventLocation}&datetime_local.lte=${daysFromNow}&per_page=1000`
+        const response = await axios.get(
+            `https://api.seatgeek.com/2/events`,
+            {
+                params: {
+                    client_id: client_id,
+                    'venue.city': eventLocation,
+                    'datetime_local.lte': daysFromNow,
+                    per_page: 1000,
+                },
+            }
         );
 
         return {
@@ -30,7 +35,7 @@ exports.handler = async (event, context) => {
                 'Access-Control-Allow-Origin': '*',
                 'Content-Type': 'application/json',
             },
-            body: JSON.stringify(await response.json()),
+            body: JSON.stringify(response.data),
         };
     } catch (error) {
         console.error('Error:', error);
